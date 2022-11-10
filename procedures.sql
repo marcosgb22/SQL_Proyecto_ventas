5e4147bb29415672ae4f5b99b96a9e1c40949385
Marcos <63947551+marcosgb22@users.noreply.github.com>
2022-11-10 01:05:23 -0300

    Update procedures.sql

--- procedures.sql
+++ procedures.sql
@@ -5,19 +5,12 @@ order by NOMBRES desc;
  
 drop procedure if exists BACKUP
 delimiter //
-
 create procedure BACKUP  (
 				 INOUT p_order varchar (50),
-                                 INOUT p_asc_desc varchar (50))
-                                 
-								 
-begin
-	
+                                 INOUT p_asc_desc varchar (50))								 
+begin	
     select * from USUARIOS
 			 order by  @p_order, @p_asc_desc;
-
-
-
-  
+ 
 end //
 delimiter ; 

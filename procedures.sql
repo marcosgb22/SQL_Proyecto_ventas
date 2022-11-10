290b3362df7085ebfd46cd4b955910a122b351e3
Marcos <63947551+marcosgb22@users.noreply.github.com>
2022-11-10 01:04:54 -0300

    Create procedures.sql

---
+++ procedures.sql
@@ -0,0 +1,23 @@
+use VENTAS;
+
+select * from USUARIOS
+order by NOMBRES desc;
+ 
+drop procedure if exists BACKUP
+delimiter //
+
+create procedure BACKUP  (
+				 INOUT p_order varchar (50),
+                                 INOUT p_asc_desc varchar (50))
+                                 
+								 
+begin
+	
+    select * from USUARIOS
+			 order by  @p_order, @p_asc_desc;
+
+
+
+  
+end //
+delimiter ; 

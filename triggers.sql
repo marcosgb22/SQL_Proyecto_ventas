b1d3c18d3832070838a5dc2e20c5ae7d67d9123f
Marcos <63947551+marcosgb22@users.noreply.github.com>
2022-11-16 21:20:47 -0300

    triggers

--- triggers.sql
+++ triggers.sql
@@ -1,4 +1,26 @@
 
+-- se genera trigger en log compras
+
+CREATE TRIGGER TR_logcompras
+ON COMPRAS
+AFTER INSERT
+AS 
+BEGIN 
+    INSERT INTO INLOG (FECHA, USER, HORA)
+    VALUES (select CURDATE(), select USER(), select NOW());
+END
+
+-- se genera trigger en mov productos
+
+CREATE TRIGGER TR_logcompras
+ON PRODUCTOS
+BEFORE UPDATE
+AS 
+BEGIN 
+    INSERT INTO INMOV (FECHA, USER, HORA)
+    VALUES (select CURDATE(), select USER(), select NOW());
+END
+
 
 -- trgger avisa si se creo un nuevo dato
 
@@ -10,6 +32,8 @@ BEGIN
     select * from inserted 
     PRINT ('se registro nueva compra')
 END
+
+
 
 
 

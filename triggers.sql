0cb1f04f54972a91bb40bde7efcfa85f99d534b9
Marcos <63947551+marcosgb22@users.noreply.github.com>
2022-11-16 21:14:10 -0300

    cambios

--- triggers.sql
+++ triggers.sql
@@ -1,1 +1,15 @@
 
+
+-- trgger avisa si se creo un nuevo dato
+
+CREATE TRIGGER TR_registronvo
+ON COMPRAS
+AFTER INSERT
+AS 
+BEGIN 
+    select * from inserted 
+    PRINT ('se registro nueva compra')
+END
+
+
+
